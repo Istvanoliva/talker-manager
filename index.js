6b1a334d0d1b60f8ab9db0228c37d88d2c235484
Istvan <olivaistvan@gmail.com>
2022-05-19 18:27:56 -0300

    Requisito 01

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const takersList = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// Requisito 01
+app.get('/talker', (_req, res) => {
+  fs.readFile(takersList).then((response) => {
+    const talkers = JSON.parse(response);
+
+    if (!talkers) return res.status(HTTP_OK_STATUS).json([]);
+    return res.status(HTTP_OK_STATUS).json(talkers);
+  });
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
-
-// teste
