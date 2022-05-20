6a7e1ea5faa5fab4509c2f124ed5893c0271f839
Istvan <olivaistvan@gmail.com>
2022-05-20 18:27:50 -0300

    Requisito 02

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -21,6 +22,20 @@ app.get('/talker', async (_req, res) => {
     if (!talkers) return res.status(HTTP_OK_STATUS).json([]);
     return res.status(HTTP_OK_STATUS).json(talkers);
   });
+
+// Requisito 02
+app.get('/talker/:id', async (req, res) => {
+  const readFile = await fs.readFile('./talker.json');
+  const talkers = JSON.parse(readFile);
+  const talkerId = talkers.find((talker) => talker.id === Number(req.params.id));
+
+  if (!talkerId) {
+    return res.status(HTTP_ERROR_STATUS).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+  res.status(HTTP_OK_STATUS).json(talkerId);
+});
 
 app.listen(PORT, () => {
   console.log('Online');
